f5e21561f8b175960e1e758631328888f31f18e5
Do_Trung_An <dotrungan19@gmail.com>
2024-01-31 11:00:35 +0700

    bai_1

---
+++ practise_1.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  String str = "đây là kết quả của buổi học thứ 2 về dart: dart basics(phần 1)...";
+  print(str);
+  print(str.toUpperCase());
+  print(str.toLowerCase());
+  print(str.replaceAll(" ", "_"));
+  print(str.substring(0,65));
+}
